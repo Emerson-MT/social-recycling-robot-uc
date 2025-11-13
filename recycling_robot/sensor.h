45e6c4f015527e336aecf2f9889bb6d0f14811de
Emerson-MT <emerson.mejia.t@gmail.com>
2025-11-13 15:38:35 -0500

    Update sensor.h

--- recycling_robot/sensor.h
+++ recycling_robot/sensor.h
@@ -1,23 +1,41 @@
 #ifndef SENSOR_H
 #define SENSOR_H
 
+#include <Wire.h>
 #include <Arduino.h>
-#include <Adafruit_VL53L0X.h>
+#include "Adafruit_VL53L0X.h"
 
 class Sensor {
-private:
-  int sda_pin;
-  int scl_pin;
-  int shut_pin;
-  int irUserPin = 2;
-  int irWastePin = 3;
+  public:
+    Sensor(uint8_t xshutPin, uint8_t address);
+    void init();
+    uint16_t readDistance();      // devuelve distancia en mm
+  private:
+    uint8_t xshutPin;
+    uint8_t address;
+    Adafruit_VL53L0X lox;
+};
 
-public:
+class UserDetector {
+  public:
+    UserDetector(Sensor& s1, Sensor& s2);
+    bool detectUser();
+  private:
+    Sensor& sensorA;
+    Sensor& sensorB;
+    unsigned long lastDetectionTime = 0;
+    unsigned long lastSwitchTime = 0;
+    bool useFirstSensor = true;
+    float detectionAccum = 0;   // tiempo acumulado en segundos
+};
 
-  Sensor(int sda_pin, int scl_pin, int shut_pin);
-  void init();
-  bool detectUser();
-  bool detectWaste();
+class WasteDetector {
+  public:
+    WasteDetector(Sensor& s);
+    bool detectWaste();
+  private:
+    Sensor& sensor;
+    const uint16_t threshold = 200; // 20 cm en mm
 };
 
 #endif
