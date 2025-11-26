850ca387e6777baeb6b3ea4b6bba7c8907462e23
PAT0359 <114365334+PAT0359@users.noreply.github.com>
2025-11-26 16:02:37 -0500

    Update servo_controller.h
    Nuevo código

--- main/servo_controller.h
+++ main/servo_controller.h
@@ -4,26 +4,72 @@
 #include <Adafruit_PWMServoDriver.h>
 #include <Arduino.h>
 
-#define SERVOMIN 150
-#define SERVOMAX 600
+#define SERVOMIN 110
+#define SERVOMAX 500
+
+// BINS
+enum BinTarget {
+    BIN_HOME = 0,   // Posición inicial/neutral (90°, 90°)
+    BIN_1 = 1,      // Cartón: BR=45°, BI=45°
+    BIN_2 = 2,      // Papel: BR=135°, BI=45°
+    BIN_3 = 3,      // Plástico: BR=45°, BI=135°
+    BIN_4 = 4       // Residuo general: BR=135°, BI=135°
+};
+
+// --- Clase Articulación (Joint) ---
+class Articulacion {
+public:
+    uint8_t canal;
+    float posicionActual;
+    float posicionObjetivo;
+    uint8_t velocidad;           // 1..100
+    unsigned long ultimoTiempo;
+
+    Articulacion(uint8_t _canal = 0);
+    
+    void setObjetivo(float nuevoAngulo, uint8_t nuevaVelocidad);
+    bool enMovimiento();
+    void actualizar();
+};
+
 
 class ServoController {
 public:
-    ServoController(int s1Channel, int s2Channel);
+    ServoController(int baseRotChannel, int baseIncChannel);
+    
     void init();
-    void setTargetAngles(int t);
-    void moveServoSmooth(int channel);
+    void setTargetBin(BinTarget bin);
     void moveAllServosSmooth();
     bool isMotionComplete();
+    void returnHome();
+    
+    // Métodos adicionales para compatibilidad
+    int angleToPulse(float ang);
 
 private:
     Adafruit_PWMServoDriver pca;
-    int servo1Channel;
-    int servo2Channel;
-    int currentAngle[16];
-    int targetAngle[16];
-    int speedPercent[16];
-    int angleToPulse(int ang);
+    
+    // Dos articulaciones principales
+    Articulacion* baseRotacion;      // BR - Base rotación
+    Articulacion* baseInclinacion;   // BI - Base inclinación
+    
+    // Canales
+    int baseRotChannel;
+    int baseIncChannel;
+    
+    // Control de secuencias
+    enum SequenceState {
+        SEQ_IDLE,
+        SEQ_ROTATE_FIRST,
+        SEQ_INCLINE_SECOND
+    };
+    
+    SequenceState currentSequence;
+    BinTarget targetBin;
+    
+    // Métodos privados
+    void startSequenceToBin(BinTarget bin);
+    void processSequence();
 };
 
 #endif
